0e6036b566d3972b960fb5151ae08a2f5d94e516
WonhyeongLee <bigfile57@naver.com>
2023-12-24 14:19:49 +0900

    Feat: intro 페이지 소개글 svg text로 구현 및 이미지영역과 레이아웃 조정

--- src/app/component/intro/Introduction.css.ts
+++ src/app/component/intro/Introduction.css.ts
@@ -1,6 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
 export const introContainer = style({
+  position: "relative",
   width: "100%",
   minHeight: "100vh",
   display: "flex",
@@ -11,25 +12,49 @@ export const introContainer = style({
 });
 
 export const introImageContainer = style({
-  width: "50%",
-  margin: "auto",
+  width: "60%",
   flex: 1,
-  border: "1px solid black",
-  padding: "1rem",
+  filter: `
+  drop-shadow(5px 0px 0px rgba(240, 46, 170, 0.4))
+  drop-shadow(10px 0px 0px rgba(240, 46, 170, 0.3))
+  drop-shadow(15px 0px 0px rgba(240, 46, 170, 0.2))
+  drop-shadow(20px 0px 0px rgba(240, 46, 170, 0.1))
+  drop-shadow(25px 0px 0px rgba(240, 46, 170, 0.05))
+`,
 });
 
 export const profileImage = style({
-  width: "70%",
   objectFit: "contain",
   minWidth: "150px",
   minHeight: "150px",
-  border: "1px solid red",
   margin: "auto",
+  clipPath: "polygon(30% 0%, 100% 0%, 70% 100%, 0% 100%)",
 });
 
 export const introTextContainer = style({
+  position: "relative",
   width: "50%",
   flex: 1,
   textAlign: "center",
-  outline: "1px solid blue",
+  // alignSelf: "flex-start",
+  marginTop: "4rem",
+  marginLeft: "2rem",
+  display: "flex",
+  flexDirection: "column",
+});
+
+// export const introImageShadow = style({
+//   filter: `
+//     drop-shadow(0px 54px 55px rgba(0, 0, 0, 0.25))
+//     drop-shadow(0px -12px 30px rgba(0, 0, 0, 0.12))
+//     drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.12))
+//     drop-shadow(0px 12px 13px rgba(0, 0, 0, 0.17))
+//     drop-shadow(0px -3px 5px rgba(0, 0, 0, 0.09))
+//   `,
+// });
+
+export const svgText = style({
+  fontSize: "40",
+  fontFamily: "Leckerli One, cursive",
+  fill: "#448aff",
 });
--- src/app/component/intro/Introduction.tsx
+++ src/app/component/intro/Introduction.tsx
@@ -8,11 +8,25 @@ const Introduction = () => {
         <img className={styles.profileImage} src={profileImage} alt='프로필' />
       </div>
       <div className={styles.introTextContainer}>
-        <span>소개텍스트</span>
         <div>
-          <p>안녕하세요</p>
-          <p>가난한 백수</p>
-          <p>이원형입니다.</p>
+          <svg viewBox='0 -100 500 300' xmlns='http://www.w3.org/2000/svg'>
+            <text x='47%' y='-5%' textAnchor='middle' fontSize={"4rem"}>
+              안녕하세요
+            </text>
+            <text x='40%' y='20%' textAnchor='middle' fontSize={"4rem"}>
+              가난한백수
+            </text>
+            <text x='39%' y='45%' textAnchor='middle' fontSize={"4rem"}>
+              이원형입니다
+            </text>
+          </svg>
+        </div>
+        <div>
+          <svg viewBox='0 0 300 100' xmlns='http://www.w3.org/2000/svg'>
+            <text x='40%' y='50%' textAnchor='middle' fontSize={"1.5rem"}>
+              멤버쉽을 소개합니다
+            </text>
+          </svg>
         </div>
       </div>
     </section>
