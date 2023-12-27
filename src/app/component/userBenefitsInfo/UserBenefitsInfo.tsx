be1f6c631a55271d599efb60d7fe06e968e4b522
WonhyeongLee <bigfile57@naver.com>
2023-12-27 20:45:22 +0900

    Feat: 혜택페이지 애니매이션 적용

--- src/app/component/userBenefitsInfo/UserBenefitsInfo.tsx
+++ src/app/component/userBenefitsInfo/UserBenefitsInfo.tsx
@@ -1,4 +1,11 @@
 import * as styles from "./UserBenefitsInfo.css";
+import gsap from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import React from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const UserBenefitsInfo = () => {
   const benefitsData = [
@@ -30,13 +37,80 @@ const UserBenefitsInfo = () => {
       ],
     },
   ];
+  const container = useRef<HTMLDivElement>(null);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+  const refs = useRef<Array<React.RefObject<HTMLDivElement>>>(
+    benefitsData.map(() => React.createRef<HTMLDivElement>())
+  );
+  const benefitsContainerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top-=270 top",
+          end: "center+=300 bottom",
+          scrub: 2,
+          markers: {
+            startColor: "green",
+            endColor: "red",
+            fontSize: "12px",
+          },
+        },
+      });
+
+      // heading 애니메이션
+      tl.fromTo(
+        headingRef.current,
+        { y: -100, opacity: 0 },
+        { y: -10, opacity: 1, delay: 0.5, duration: 2, ease: "power1.inOut" },
+        "-=0.5"
+      );
+
+      // benefitsContainer 애니메이션
+      tl.fromTo(
+        benefitsContainerRef.current,
+        { y: 100, opacity: 0 },
+        { y: 20, opacity: 1, delay: 0.5, duration: 2, ease: "power1.inOut" },
+        "-=1"
+      );
+
+      // li[0] 애니메이션
+      tl.fromTo(
+        refs.current[0].current,
+        { xPercent: 100, opacity: 0 },
+        { xPercent: 0, opacity: 1, duration: 2 },
+        "-=2"
+      );
+
+      // li[1] 애니메이션
+      tl.fromTo(
+        refs.current[1].current,
+        { xPercent: 0, opacity: 0 },
+        { xPercent: 0, opacity: 1, duration: 2, ease: "power1.out" },
+        "-=2"
+      );
+
+      // li[2] 애니메이션
+      tl.fromTo(
+        refs.current[2].current,
+        { xPercent: -100, opacity: 0 },
+        { xPercent: 0, opacity: 1, duration: 2, ease: "power1.out" },
+        "-=2"
+      );
+    },
+    { scope: container }
+  );
 
   return (
-    <section className={styles.UserBenefitsInfoContainer}>
-      <h2 className={styles.UserBenefitsHeading}>멤버쉽 등급에 따른 혜택</h2>
-      <div className={styles.UserBenefitsGridContainer}>
-        {benefitsData.map(({ title, benefits }) => (
-          <div className={styles.UserBenefitsGridItem} key={title}>
+    <section ref={container} className={styles.UserBenefitsInfoContainer}>
+      <h2 ref={headingRef} className={styles.UserBenefitsHeading}>
+        멤버쉽 등급에 따른 혜택
+      </h2>
+      <div className={styles.UserBenefitsGridContainer} ref={benefitsContainerRef}>
+        {benefitsData.map(({ title, benefits }, index) => (
+          <div className={styles.UserBenefitsGridItem} key={title} ref={refs.current[index]}>
             <span className={styles.UserBenefitsTag}>{title}</span>
             <ul className={styles.UserBenefitsList}>
               {benefits.map(({ id, benefit }) => (
