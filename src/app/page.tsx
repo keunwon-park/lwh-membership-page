5caac3f1f943305e155e63312f7153f2cfc06842
WonhyeongLee <bigfile57@naver.com>
2024-02-07 01:59:08 +0900

    Fix: 새로고침 시 스크롤 위치로 이동하는 문제 수정
    - ScrollTriger의 config중 clearScrollMemory 옵션을 사용해 "manual"로 설정해서 새로고침 시 스크롤 위치를 초기화하지 않도록 수정
    - 새로고침 시 React 컴포넌트가 언마운트될 때 ScrollTriger.kill() 메서드를 호출해서 ScrollTriger 인스턴스를 제거하도록 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+import { useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import RootLayout from "./layout";
 import Introduction from "./component/intro/Introduction";
 import CommonBenefits from "./component/membershipBenefits/CommonBenefits";
 import MembershipBenefits from "./component/membershipBenefits/MembershipBenefits";
 import Checkout from "./component/membershipPayment/Checkout";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import * as styles from "./MainPage.css";
-gsap.registerPlugin(ScrollTrigger);
-ScrollTrigger.config({autoRefreshEvents: "visibilitychange,DOMContentLoaded,load"});
-ScrollTrigger.clearScrollMemory();
-window.history.scrollRestoration = "manual";
 
 const MainPage = () => {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.config({
+      autoRefreshEvents: "visibilitychange,DOMContentLoaded,load",
+    });
+    ScrollTrigger.clearScrollMemory();
+    window.history.scrollRestoration = "manual";
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
 
   return (
     <RootLayout>
